013088c09a0f86554a0f07fad5116f4a1b755250
Davide Tavella <tavella01@gmail.com>
2021-11-22 15:06:58 +0100

    al click genera 1 mail

--- js/script.js
+++ js/script.js
@@ -3,21 +3,44 @@ const app = new Vue({
    el: '#app',
  
    data: {
-     
+     numero: 10,
+     isLoading: true,
+     httpError: false,
+     randomMail:[],
      
    },
-   
-   mounted(){
- 
-     
- 
-   },
-
    methods:{
  
+      generateMail(){
+       this.isLoading = true;
+ 
+       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+       .then((response) =>{
+
+         // console.log(response);
+ 
+         this.mail  = response.data.response;
+
+         console.log(this.mail);
+ 
+     
+         this.isLoading = false;
+       })
+       .catch((error) =>{
+         console.log('ERRORE',error);
+         
+         this.httpError = true;
+       })
+     }
  
    },
-   
+   mounted(){
 
+      // richiamo la funzione
+     this.generateMail();
+     
+ 
+   }
+ 
  
  })
